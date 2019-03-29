a3af2c08a6f1ea754651644c2fc593db3239bdf7
scottwallace23 <47538155+scottwallace23@users.noreply.github.com>
2019-03-29 12:32:59 -0400

    GUI

--- src/GUI.scala
+++ src/GUI.scala
@@ -1,48 +1,57 @@
-package gui
 
-import javafx.scene.input.{KeyCode, KeyEvent, MouseEvent}
+import Backend.circles.{Food, Player}
+import javafx.scene.input.{KeyCode, KeyEvent}
 import scalafx.animation.AnimationTimer
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
 import scalafx.scene.paint.Color
-import scalafx.scene.shape.{Circle, Rectangle, Shape}
+import scalafx.scene.shape.{Circle, Shape}
 import scalafx.scene.{Group, Scene}
 
-object GUI2D extends JFXApp {
+object GUI extends JFXApp {
 
-  val windowWidth: Double = 800
-  val windowHeight: Double = 600
+  var sceneWidth: Double = 500
+  var sceneHeight: Double = 500
 
   var playerCircleRadius: Double = 20
   val playerSpeed: Double = 10
 
 
-  var sceneGraphics: Group = new Group {}
+  var g: Group = new Group {}
 
   val player: Circle = new Circle {
-    centerX = Math.random() * windowWidth
-    centerY = Math.random() * windowHeight
-    radius = playerCircleRadius
+    centerX = Math.random() * sceneWidth
+    centerY = Math.random() * sceneHeight
+    radius = Player.size
+    fill = Color.Blue
+  }
+  val food: Circle = new Circle {
+    centerX = Math.random() * sceneWidth
+    centerY = Math.random() * sceneHeight
+    radius = Food.size
     fill = Color.Green
   }
-  sceneGraphics.children.add(player)
+  g.children.addAll(food)
+  g.children.add(player)
 
 
-  def keyPressed(keyCode: KeyCode): Unit = {
-    keyCode.getName match {
+  def keyPressed(key: KeyCode): Unit = {
+    key.getName match {
       case "W" => player.translateY.value -= playerSpeed
       case "A" => player.translateX.value -= playerSpeed
       case "S" => player.translateY.value += playerSpeed
       case "D" => player.translateX.value += playerSpeed
       case _ =>
     }
+
+
   }
 
 
   this.stage = new PrimaryStage {
-    this.title = "2D Graphics"
-    scene = new Scene(windowWidth, windowHeight) {
-      content = List(sceneGraphics)
+    this.title = "Agar.io"
+    scene = new Scene(sceneWidth, sceneHeight) {
+      content = List(g)
 
       // add an EventHandler[KeyEvent] to control player movement
       addEventHandler(KeyEvent.KEY_PRESSED, (event: KeyEvent) => keyPressed(event.getCode))
@@ -50,7 +59,6 @@ object GUI2D extends JFXApp {
       // define a function for the action timer (Could also use a method)
       // Rotate all rectangles (relies on frame rate. lag will slow rotation)
       val update: Long => Unit = (time: Long) => {
-
       }
 
       // Start Animations. Calls update 60 times per second (takes update as an argument)
