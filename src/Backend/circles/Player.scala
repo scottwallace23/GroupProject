45fb5b23092f6f571f8b6c0b775ebe0ae322b97a
scottwallace23 <47538155+scottwallace23@users.noreply.github.com>
2019-03-29 13:01:34 -0400

    Updated classes and objects

--- src/Backend/circles/Player.scala
+++ src/Backend/circles/Player.scala
@@ -1,57 +1,48 @@
 package Backend.circles
 
+import scalafx.scene.paint.Color
+
 import scala.util.Random
 
 
 object Player extends Circles {
-  this.size = 10  //Initial size for player circle
-  def setXLoc(): Int = {
-    val xmax = 1000
-    val rand = new scala.util.Random
-    val xloc = rand.nextInt(xmax)
-    xloc
-  }
-  def setYLoc(): Int = {
-    val ymax = 1000
-    val rand = new scala.util.Random
-    val yloc = rand.nextInt(ymax)
-    yloc
-  }
-  var playerLocationX = setXLoc()
-  var playerLocationY = setYLoc() //initializes location at a random (x,y) coordinate
+  this.size = 20  //Initial size for player circle
+  var playerLocationX = centerX
+  var playerLocationY = centerY
 
-/* def randomColor(): String ={      //Returns the random color for the player circle.
+  def randomColor(): Color = {      //Returns the random color for the player circle.
+
     val arrayColors: Array[Int] = Array(0,1,2,3)
     val randomNum = Random.shuffle(arrayColors.toList).head
 
     if(randomNum == 0){
-      this.color = "red"
+      this.color = Color.Red
     }
     else if(randomNum == 1){
-      this.color = "blue"
+      this.color = Color.Blue
     }
     else if(randomNum == 2){
-      this.color = "green"
+      this.color = Color.Green
     }
     else if(randomNum == 3){
-      this.color = "yellow"
+      this.color = Color.Yellow
     }
-   this.color
+    this.color
   }
-*/
 
   def eatFood(): Int = {
+    if(Food.size <= 0){
+      Food.size = 0
+      Food.size
+    }
     if(Player.playerLocationX == Food.foodLocationX && Player.playerLocationY == Food.foodLocationY){
       Player.size += Food.size  //Adds to the player's size by the size of the food
-      Food.size -= 1  //Subtracts the size of the food which basically destroys the food once its eaten
+      Food.size -= Food.size  //Subtracts the size of the food which basically destroys the food once its eaten
     }
     Player.size
 
   }
 
-  def main(args: Array[String]): Unit = {
-    //println(randomColor())
-  }
 
 
 
